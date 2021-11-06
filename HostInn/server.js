4cbe5e8deed50548c4a3a2847b6d1b5609ff3038
Oswaldo <oswaldo@estudiantec.cr>
2021-11-05 20:22:07 -0600

    Inicio de proyecto

---
+++ HostInn/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.static("express"));
+app.use("/public", express.static(__dirname + "/public"));
+
+// sitio por defecto del sitio web
+app.use('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/express/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
