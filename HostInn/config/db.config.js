38a7f2fd3a38c87c0cebe9ef6cf2295d7f6d718b
Leemxch <61258635+Leemxch@users.noreply.github.com>
2021-11-11 12:18:45 -0600

    CRUD básico

---
+++ HostInn/config/db.config.js
@@ -0,0 +1,16 @@
+'use strict';
+var mysql = require('mysql');
+//local mysql db connection
+const dbConn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'hostinn'
+});
+
+dbConn.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected!");
+});
+
+module.exports = dbConn;
--- HostInn/src/model.js
+++ HostInn/src/model.js
@@ -1,1 +1,23 @@
-var dbConn = require('../config/db.config');
+const dbConn = require('../config/db.config');
+
+/*var sql = "insert into Cuenta (IdCuenta, Username, Contraseña, Estado) values (2, 'meh', 'asd', 1)";
+  dbConn.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+
+dbConn.query("SELECT * FROM Cuenta", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+*/
+var sql = "UPDATE Cuenta SET Estado = " + '2' +  " WHERE IdCuenta = " + '5';
+dbConn.query(sql, function (err, result) {
+  if (err) throw err;
+  console.log(result.affectedRows + " record(s) updated");
+});
+
+dbConn.query("SELECT * FROM Cuenta", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
