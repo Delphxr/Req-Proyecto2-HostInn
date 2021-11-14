3afacb8318459c29dd00051f7456e6df32924876
Oswaldo <oswaldo@estudiantec.cr>
2021-11-13 19:25:46 -0600

    hacer click en hotel redirecciona a pagina con id del hotel

--- HostInn/server.js
+++ HostInn/server.js
@@ -15,13 +15,16 @@ app.use("/views", express.static(__dirname + "/views"));
 // sitio por defecto del sitio web
 app.get('/', function (req, res) {
 
+
+
+    // nota: se recomiendan imagenes con ratio de 3:2
     var hoteles = [
-        {nombre: "Hotel el Pepe",  ubicacion: "Playa Sucia", estrellas: 5, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1},
-        {nombre: "Hotel el Buen Dormir",  ubicacion: "Volcan Poas", estrellas: 4, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1},
-        {nombre: "Hotel San Juan",  ubicacion: "Playa San Juan", estrellas: 5, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1},
-        {nombre: "Hotel Doña Checha",  ubicacion: "Rio Virilla", estrellas: 1, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1},
-        {nombre: "Hotel Invisible",  ubicacion: "Playa que no existe", estrellas: 5, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:0},
-        {nombre: "Hotel visible",  ubicacion: "Playa que si existe", estrellas: 3, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1}
+        {id: 1, nombre: "Hotel el Pepe",  ubicacion: "Playa Sucia", estrellas: 5, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1},
+        {id: 2, nombre: "Hotel el Buen Dormir",  ubicacion: "Volcan Poas", estrellas: 4, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/200/400", Estado:1},
+        {id: 3, nombre: "Hotel San Juan",  ubicacion: "Playa San Juan", estrellas: 5, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1},
+        {id: 4, nombre: "Hotel Doña Checha",  ubicacion: "Rio Virilla", estrellas: 1, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1},
+        {id: 5, nombre: "Hotel Invisible",  ubicacion: "Playa que no existe", estrellas: 5, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:0},
+        {id: 6, nombre: "Hotel visible",  ubicacion: "Playa que si existe", estrellas: 3, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1}
     ]
 
     res.render(path.join(__dirname + '/views/pages/homepage.ejs'), 
@@ -31,13 +34,16 @@ app.get('/', function (req, res) {
 
 
 // sitio habitaciones
-app.get('/rooms', function (req, res) {
+// :hotelId es el parametro con el id del hotel
+app.get('/rooms/:hotelId', function (req, res) {
 
+    var hotelId = req.params.hotelId
         
-    var hotel = {nombre: "Hotel el Pepe",  ubicacion: "Playa Sucia", estrellas: 5, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1}
+    var hotel = {nombre: "el id del hotel es: " + hotelId,  ubicacion: "Playa Sucia", estrellas: 5, Descripcion: "este es un hotel", rutaImagen:"https://picsum.photos/450/300", Estado:1}
 
     var habitaciones = [
         {nombre: "Suite Matrimonial",  tipo: "Suite", camas: 1, capacidad: 3, Descripcion: "esta es una habitacion", rutaImagen:"https://picsum.photos/450/300", Estado:1},
+        {nombre: "Suite Matrimonial",  tipo: "Suite", camas: 1, capacidad: 3, Descripcion: "esta es una habitacion", rutaImagen:"https://picsum.photos/450/300", Estado:1}
     ]
 
     res.render(path.join(__dirname + '/views/pages/rooms.ejs'), 
